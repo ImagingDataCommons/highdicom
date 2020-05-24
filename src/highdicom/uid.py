74a9ff11c4ca7152f5b3efcc8fa9679a3ce9319b
hackermd <hackermd@protonmail.com>
2020-05-23 21:44:52 -0400

    Fix type annotation of uid class constructor

--- src/highdicom/uid.py
+++ src/highdicom/uid.py
@@ -9,7 +9,7 @@ class UID(pydicom.uid.UID):
 
     """Unique DICOM identifier with a highdicom-specific UID prefix."""
 
-    def __new__(cls):
+    def __new__(cls: type) -> None:
         prefix = '1.2.826.0.1.3680043.10.511.3.'
         identifier = pydicom.uid.generate_uid(prefix=prefix)
         super().__new__(cls, identifier)
