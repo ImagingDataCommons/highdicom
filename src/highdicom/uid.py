61f91ef004ebdf851b5092bb963ef6c5378c7e2d
hackermd <hackermd@protonmail.com>
2020-05-23 21:53:13 -0400

    Fix return value of uid class __new__ method

--- src/highdicom/uid.py
+++ src/highdicom/uid.py
@@ -9,7 +9,7 @@ class UID(pydicom.uid.UID):
 
     """Unique DICOM identifier with a highdicom-specific UID prefix."""
 
-    def __new__(cls: type) -> None:
+    def __new__(cls: type) -> str:
         prefix = '1.2.826.0.1.3680043.10.511.3.'
         identifier = pydicom.uid.generate_uid(prefix=prefix)
-        super().__new__(cls, identifier)
+        return super().__new__(cls, identifier)
