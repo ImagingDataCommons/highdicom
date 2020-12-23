e686cf048e34cddde9921edf330fdf76ce89701f
hackermd <hackermd@protonmail.com>
2020-12-23 13:21:36 -0500

    Exlude newer problematic pydicom versions

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setuptools.setup(
     package_dir={'': 'src'},
     python_requires='>=3.6',
     install_requires=[
-        'pydicom>=1.4,!=2.1.0,!=2.1.1',
+        'pydicom>=1.4,<2.1.0',
         'numpy>=1.19',
         'pillow>=6.0'
     ],
