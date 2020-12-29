19300da298f2eab852fb53f9daba36fb041f269f
hackermd <hackermd@protonmail.com>
2020-12-28 19:06:33 -0500

    Apply transformations to batch of coordinates

--- src/highdicom/spatial.py
+++ src/highdicom/spatial.py
@@ -23,8 +23,10 @@ def create_rotation_matrix(
         3 x 3 rotation matrix
 
     """
-    row_cosines = np.array(image_orientation[:3])
-    column_cosines = np.array(image_orientation[3:])
+    if len(image_orientation) != 6:
+        raise ValueError('Argument "image_orientation" must have length 6.')
+    row_cosines = np.array(image_orientation[:3], dtype=float)
+    column_cosines = np.array(image_orientation[3:], dtype=float)
     n = np.cross(row_cosines.T, column_cosines.T)
     return np.column_stack([
         row_cosines,
@@ -67,6 +69,8 @@ def compute_rotation(
         reference coordinate system by a rotation within the image plane.
 
     """
+    if len(image_orientation) != 6:
+        raise ValueError('Argument "image_orientation" must have length 6.')
     if (image_orientation[2] != 0.0) or (image_orientation[5] != 0.0):
         raise ValueError(
             "The provided image orientation is not related to the frame of "
@@ -117,16 +121,38 @@ def build_transform(
     numpy.ndarray
         4 x 4 affine transformation matrix
 
+    Raises
+    ------
+    TypeError
+        When any of the arguments is not a sequence.
+    ValueError
+        When any of the arguments has an incorrect length.
+
     """
+    if not isinstance(image_position, Sequence):
+        raise TypeError('Argument "image_position" must be a sequence.')
+    if len(image_position) != 3:
+        raise ValueError('Argument "image_position" must have length 3.')
+    if not isinstance(image_orientation, Sequence):
+        raise TypeError('Argument "image_orientation" must be a sequence.')
+    if len(image_orientation) != 6:
+        raise ValueError('Argument "image_orientation" must have length 6.')
+    if not isinstance(pixel_spacing, Sequence):
+        raise TypeError('Argument "pixel_spacing" must be a sequence.')
+    if len(pixel_spacing) != 2:
+        raise ValueError('Argument "pixel_spacing" must have length 2.')
+
     x_offset = float(image_position[0])
     y_offset = float(image_position[1])
     z_offset = float(image_position[2])
-    translation = np.array([x_offset, y_offset, z_offset])
+    translation = np.array([x_offset, y_offset, z_offset], dtype=float)
+
     rotation = create_rotation_matrix(image_orientation)
     column_spacing = float(pixel_spacing[0])  # column direction (between rows)
     row_spacing = float(pixel_spacing[1])  # row direction (between columns)
     rotation[:, 0] *= row_spacing
     rotation[:, 1] *= column_spacing
+
     # 4x4 transformation matrix
     return np.row_stack(
         [
@@ -175,7 +201,29 @@ def build_inverse_transform(
     numpy.ndarray
         4 x 4 affine transformation matrix
 
+    Raises
+    ------
+    TypeError
+        When `image_position`, `image_orientation` or `pixel_spacing` is not a
+        sequence.
+    ValueError
+        When `image_position`, `image_orientation` or `pixel_spacing` has an
+        incorrect length.
+
     """
+    if not isinstance(image_position, Sequence):
+        raise TypeError('Argument "image_position" must be a sequence.')
+    if len(image_position) != 3:
+        raise ValueError('Argument "image_position" must have length 3.')
+    if not isinstance(image_orientation, Sequence):
+        raise TypeError('Argument "image_orientation" must be a sequence.')
+    if len(image_orientation) != 6:
+        raise ValueError('Argument "image_orientation" must have length 6.')
+    if not isinstance(pixel_spacing, Sequence):
+        raise TypeError('Argument "pixel_spacing" must be a sequence.')
+    if len(pixel_spacing) != 2:
+        raise ValueError('Argument "pixel_spacing" must have length 2.')
+
     x_offset = float(image_position[0])
     y_offset = float(image_position[1])
     z_offset = float(image_position[2])
@@ -201,70 +249,102 @@ def build_inverse_transform(
 
 
 def apply_transform(
-        coordinate: Sequence[float],
+        coordinates: np.ndarray,
         affine: np.ndarray
-    ) -> Tuple[float, float, float]:
-    """Applies an affine transformation matrix to a pixel matrix coordinate
-    to obtain a coordinate in the three-dimensional frame of reference.
+    ) -> np.ndarray:
+    """Applies an affine transformation matrix to a batch of pixel matrix
+    coordinates to obtain the corresponding coordinates in the
+    three-dimensional frame of reference.
 
     Parameters
     ----------
-    coordinate: Sequence[float]
-        (Column, Row) coordinate in the Total Pixel Matrix in pixel unit at
-        sub-pixel resolution.
+    coordinates: numpy.ndarray
+        Array of (Column, Row) coordinates in the Total Pixel Matrix in pixel
+        unit at sub-pixel resolution. Array should have shape ``(n, 2)``, where
+        *n* is the number of coordinates, the first column represents the
+        *Column* values and the second column represents the *Row* values.
     affine: numpy.ndarray
         4 x 4 affine transformation matrix
 
     Returns
     -------
-    Tuple[float, float, float]
-        (X, Y, Z) coordinate in the coordinate system defined by the
-        Frame of Reference
+    numpy.ndarray
+        Array of (X, Y, Z) coordinates in the coordinate system defined by the
+        Frame of Reference. Array has shape ``(n, 3)``, where
+        *n* is the number of coordinates, the first column represents the *X*
+        offsets, the second column represents the *Y* offsets and the third
+        column represents the *Z* offsets
+
+    Raises
+    ------
+    ValueError
+        When `coordinates` has incorrect shape.
 
     """
-    column_offset = float(coordinate[0])
-    row_offset = float(coordinate[1])
-    pixel_matrix_coordinate = np.array([[column_offset, row_offset, 0.0, 1.0]])
-    physical_coordinate = np.dot(affine, pixel_matrix_coordinate.T)
-    return tuple(physical_coordinate[:3].flatten().tolist())
+    if coordinates.shape[1] != 2:
+        raise ValueError(
+            'Argument "coordinates" must be a two-dimensional array '
+            'with shape [n, 2].'
+        )
+    pixel_matrix_coordinates = np.row_stack([
+        coordinates.T,
+        np.zeros((coordinates.shape[0], ), dtype=float),
+        np.ones((coordinates.shape[0], ), dtype=float),
+    ])
+    physical_coordinates = np.dot(affine, pixel_matrix_coordinates)
+    return physical_coordinates[:3, :].T
 
 
 def apply_inverse_transform(
-        coordinate: Sequence[float],
+        coordinates: np.array,
         affine: np.ndarray
-    ) -> Tuple[float, float, float]:
-    """Applies the inverse of an affine transformation matrix to a
-    coordinate in the three-dimensional frame of reference to obtain a pixel
-    matrix coordinate.
+    ) -> np.array:
+    """Applies the inverse of an affine transformation matrix to a batch of
+    coordinates in the three-dimensional frame of reference to obtain the
+    corresponding pixel matrix coordinates.
 
     Parameters
     ----------
-    coordinate: Sequence[float]
-        (X, Y, Z) coordinate in the coordinate system defined by the
-        Frame of Reference
+    coordinates: numpy.ndarray
+        Array of (X, Y, Z) coordinates in the coordinate system defined by the
+        Frame of Reference. Array should have shape ``(n, 3)``, where
+        *n* is the number of coordinates, the first column represents the *X*
+        offsets, the second column represents the *Y* offsets and the third
+        column represents the *Z* offsets
     affine: numpy.ndarray
         4 x 4 affine transformation matrix
 
     Returns
     -------
-    Tuple[float, float, float]
-        (Column, Row, Slice) coordinate, where the `Column` and `Row` values
-        relate to the Total Pixel Matrix in pixel units at sub-pixel resolution
-        and the `Slice` value represents the signed distance of the input
-        coordinate in the direction normal to the plane of the Total Pixel
-        Matrix represented in units of the given spacing between slices.
-        The `Row` and `Column` values are constrained by the dimension of the
+    numpy.ndarray
+        Array of (Column, Row, Slice) coordinates, where the
+        `Column` and `Row` offsets relate to the Total Pixel Matrix in pixel
+        units at sub-pixel resolution and the `Slice` offset represents the
+        signed distance of the input coordinate in the direction normal to the
+        plane of the Total Pixel Matrix represented in units of the given
+        spacing between slices.
+        The `Row` and `Column` offsets are constrained by the dimension of the
         Total Pixel Matrix. Note, however, that in general, the resulting
         coordinate may not lie within the imaging plane, and consequently the
-        `Slice` value may be non-zero.
+        `Slice` offset may be non-zero.
+
+    Raises
+    ------
+    ValueError
+        When `coordinates` has incorrect shape.
 
     """
-    x = float(coordinate[0])
-    y = float(coordinate[1])
-    z = float(coordinate[2])
-    physical_coordinate = np.array([[x, y, z, 1.0]])
-    pixel_matrix_coordinate = np.dot(affine, physical_coordinate.T)
-    return tuple(pixel_matrix_coordinate[:3].flatten().tolist())
+    if coordinates.shape[1] != 3:
+        raise ValueError(
+            'Argument "coordinates" must be a two-dimensional array '
+            'with shape [n, 3].'
+        )
+    physical_coordinates = np.row_stack([
+        coordinates.T,
+        np.ones((coordinates.shape[0], ), dtype=float)
+    ])
+    pixel_matrix_coordinates = np.dot(affine, physical_coordinates)
+    return pixel_matrix_coordinates[:3, :].T
 
 
 def map_pixel_into_coordinate_system(
@@ -309,13 +389,26 @@ def map_pixel_into_coordinate_system(
     ``apply_transform()``. When mapping a large number of coordinates,
     consider using the underlying functions directly for speedup.
 
+    Raises
+    ------
+    TypeError
+        When `image_position`, `image_orientation` or `pixel_spacing` is not a
+        sequence.
+    ValueError
+        When `image_position`, `image_orientation` or `pixel_spacing` has an
+        incorrect length.
+
     """
     affine = build_transform(
         image_position=image_position,
         image_orientation=image_orientation,
         pixel_spacing=pixel_spacing
     )
-    return apply_transform(coordinate, affine=affine)
+    transformed_coordinates = apply_transform(
+        coordinates=np.array([coordinate], dtype=float),
+        affine=affine
+    )
+    return tuple(transformed_coordinates[0, :].tolist())
 
 
 def map_coordinate_into_pixel_matrix(
@@ -354,20 +447,30 @@ def map_coordinate_into_pixel_matrix(
     Returns
     -------
     Tuple[float, float, float]
-        (Column, Row, Slice) coordinate. ``Column`` and ``Row`` are pixel
-        coordinates in the Total Pixel Matrix, ``Slice`` represents the signed
+        (Column, Row, Slice) coordinate, where `Column` and `Row` are pixel
+        coordinates in the Total Pixel Matrix, `Slice` represents the signed
         distance of the input coordinate in the direction normal to the plane
         of the Total Pixel Matrix represented in units of the given spacing
-        between slices. If the ``Slice`` coordinate is 0.0, then the input
+        between slices. If the `Slice` offset is ``0.0``, then the input
         coordinate lies in the imaging plane, otherwise it lies off the plane
-        of the Total Pixel Matrix and ``Column`` and ``Row`` may be interpreted
-        as the projections of the input coordinate onto the imaging plane.
+        of the Total Pixel Matrix and `Column` and `Row` offsets may be
+        interpreted as the projections of the input coordinate onto the
+        imaging plane.
 
     Note
     ----
     This function is a convenient wrapper around ``build_inverse_transform()``
     and ``apply_inverse_transform()``. When mapping a large number of
     coordinates, consider using the underlying functions directly for speedup.
+
+    Raises
+    ------
+    TypeError
+        When `image_position`, `image_orientation` or `pixel_spacing` is not a
+        sequence.
+    ValueError
+        When `image_position`, `image_orientation` or `pixel_spacing` has an
+        incorrect length.
 
     """
     affine = build_inverse_transform(
@@ -376,4 +479,8 @@ def map_coordinate_into_pixel_matrix(
         pixel_spacing=pixel_spacing,
         spacing_between_slices=spacing_between_slices
     )
-    return apply_inverse_transform(coordinate, affine=affine)
+    transformed_coordinates = apply_inverse_transform(
+        coordinates=np.array([coordinate], dtype=float),
+        affine=affine
+    )
+    return tuple(transformed_coordinates[0, :].tolist())
