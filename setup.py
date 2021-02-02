a5cfc4d5fffbbcef7eb93adf8fc5073db7e0a881
hackermd <hackermd@protonmail.com>
2021-02-02 13:44:38 -0500

    Add support for Python 3.9

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ setuptools.setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Development Status :: 4 - Beta',
     ],
     include_package_data=True,
