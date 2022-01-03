1e92d760f4d04e1f2e3e4116369d6a7c78187e1e
hackermd <hackermd@protonmail.com>
2022-01-03 13:46:29 -0500

    Increase package version

--- setup.py
+++ setup.py
@@ -31,6 +31,8 @@ setuptools.setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         'Development Status :: 4 - Beta',
     ],
     include_package_data=True,
