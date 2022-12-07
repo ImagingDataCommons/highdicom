d2f051362c36bf6cbeec8dde345eefcbd69e8e8b
Chris Bridge <chrisbridge44@googlemail.com>
2022-12-07 09:14:52 -0500

    Update src/highdicom/seg/sop.py
    Co-authored-by: Markus D. Herrmann <hackermd@users.noreply.github.com>

--- src/highdicom/seg/sop.py
+++ src/highdicom/seg/sop.py
@@ -2034,8 +2034,8 @@ class Segmentation(SOPClass):
                 if not rescale_fractional:
                     raise ValueError(
                         'In order to combine segments of a FRACTIONAL '
-                        'segmentation image, rescale_fractional must be '
-                        'set to True.'
+                        'segmentation image, argument "rescale_fractional" '
+                        'must be set to True.'
                     )
                 # Combining fractional segs is only possible if there are
                 # two unique values in the array: 0 and MaximumFractionalValue
