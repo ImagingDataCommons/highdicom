3761f87dad3fd04e1642dc0e646a308f186a48d6
kyinhub <kevinpyin@gmail.com>
2026-07-25 11:08:50 -0700

    fix: validate image reader frame indices
    Signed-off-by: kyinhub <kevinpyin@gmail.com>

--- src/highdicom/io.py
+++ src/highdicom/io.py
@@ -693,12 +693,18 @@ class ImageFileReader:
 
         Raises
         ------
+        ValueError
+            When `index` is less than 0 or greater than or equal to the number
+            of frames.
         OSError
             When frame could not be read
 
         """
-        if index > self.number_of_frames:
-            raise ValueError('Frame index exceeds number of frames in image.')
+        if index < 0 or index >= self.number_of_frames:
+            raise ValueError(
+                'Frame index must be greater than or equal to 0 and less '
+                'than the number of frames in the image.'
+            )
         logger.debug(f'read frame #{index}')
 
         frame_offset = self._offset_table[index]
@@ -751,6 +757,9 @@ class ImageFileReader:
 
         Raises
         ------
+        ValueError
+            When `index` is less than 0 or greater than or equal to the number
+            of frames.
         OSError
             When frame could not be read
 
