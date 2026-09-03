faca0ba137c2afe3c6d383e4c5199330afa5f5e6
Mason C. Cleveland <104479423+mccle@users.noreply.github.com>
2026-09-03 17:17:01 -0400

    Update src/highdicom/volume.py
    Co-authored-by: Chris Bridge <chrisbridge44@googlemail.com>

--- src/highdicom/volume.py
+++ src/highdicom/volume.py
@@ -4259,7 +4259,7 @@ class Volume(_VolumeBase):
         Parameters
         ----------
         convert_to_ras: bool
-            Whether to conver from 'LPS' to 'RAS' convention. Defaults to True.
+            Whether to convert the affine matrix from 'LPS' to 'RAS' convention.
         ensure_channel_first: bool
             Whether to convert to a channel first metatensor. Defaults to False.
 
